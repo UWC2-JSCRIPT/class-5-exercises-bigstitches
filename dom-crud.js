f32e3030b64d3b659d5a07efeec60ea7a8abac1f
Eileen B <eileen.e.beck@gmail.com>
2022-11-15 20:43:24 -0800

    crud done

--- dom-crud.js
+++ dom-crud.js
@@ -1,14 +1,19 @@
 // Create a new <a> element containing the text "Buy Now!" 
-// with an id of "cta" after the last <p>
-
+// with an id of "cta" 
+let buyNow = document.createElement('a');
+buyNow.innerHTML = "Buy Now!";
+buyNow.id = 'cta';
+// after the last <p>
+document.getElementsByTagName('p')[(document.getElementsByTagName('p').length - 1)].append(buyNow);
 
 // Access (read) the data-color attribute of the <img>,
 // log to the console
-
+Array.from(document.getElementsByTagName("img")).forEach(item => console.log(`color: ${item.getAttribute('data-color')}`));
 
 // Update the third <li> item ("Turbocharged"), 
 // set the class name to "highlight"
-
+document.getElementsByTagName('li')[2].className = "highlight";
 
 // Remove (delete) the last paragraph
 // (starts with "Available for purchase now…")
+document.getElementsByTagName('p')[(document.getElementsByTagName('p').length - 1)].remove();
