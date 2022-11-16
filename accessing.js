24e2088b33f5dc8afdd663f139f3f234f881ba97
Eileen B <eileen.e.beck@gmail.com>
2022-11-15 19:26:39 -0800

    changed accessing, ready for final

--- accessing.js
+++ accessing.js
@@ -1,5 +1,15 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
+document.getElementById("weather-head").innerHTML = 'February 10 Weather Forecast, Seattle';
 
 // Change the styling of every element with class "sun" to set the color to "orange"
+Array.from(document.getElementsByClassName("sun")).forEach(item => item.style.color = "orange");
+
+/*
+var elements = document.getElementsByClassName('sun'); // get all elements of class sun
+	for(var i = 0; i < elements.length; i++){
+		elements[i].style.color = "orange";
+	}
+*/
 
 // Change the class of the second <li> from to "sun" to "cloudy"
+document.getElementsByTagName("li")[1].className = "cloudy";
