023cf99e3bb7f8b5c5a7309e8c8fadfd53816d61
Eileen B <eileen.e.beck@gmail.com>
2022-11-16 16:19:40 -0800

    pluses and minuses html and js

--- plusesAndMinuses.js
+++ plusesAndMinuses.js
@@ -1,2 +1,20 @@
 // When a user clicks the + element, the count should increase by 1 on screen.
+//var beginningValue = parseFloat(document.getElementById("user-Value").innerHTML);
+//var negativeOkay
+const addfunction = function () {
+    let newValue = parseFloat(document.getElementById("user-Value").innerHTML);
+    document.getElementById("user-Value").innerHTML = (newValue + 1);
+}
+
+const subtractfunction = function () {
+    let newValue = parseFloat(document.getElementById("user-Value").innerHTML);
+    document.getElementById("user-Value").innerHTML = (newValue - 1);
+}
+//document.getElementById("radio-IDYES").addEventListener("click", function(){ alert("Hello World!"); });
 // When a user clicks the – element, the count should decrease by 1 on screen.
+//document.getElementById("radio-IDNO").addEventListener("click", function(){ alert("Hello World!"); });
+
+document.getElementById('add-ID').addEventListener('click', addfunction);
+
+document.getElementById('minus-ID').addEventListener('click', subtractfunction);
+
